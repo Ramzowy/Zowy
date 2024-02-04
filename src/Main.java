8cbc797e58bf5b62032bfc2f0cd910b2b2f1a910
Rahim <ramzowy@gmail.com>
2024-02-03 21:02:55 -0500

    Lab_04

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+//import static javax.swing.text.html.HTML.Tag.S;
+
+//import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+     int purchasePrice = 500;
+     final double SALES_TAX = 0.05;
+     double totalAmount = 0;
+     double taxAmount;
+
+               taxAmount = purchasePrice * SALES_TAX;
+               totalAmount = taxAmount + purchasePrice;
+
+
+             System.out.println(totalAmount);
+             System.out.println(taxAmount);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
